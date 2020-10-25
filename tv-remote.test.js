e908ee2c0a6120a766610bf53d815bfef18400e0
Eva Coffield <eva@darcoff.com>
2020-10-25 09:23:37 +0000

    Initial commit

---
+++ tv-remote.test.js
@@ -0,0 +1,21 @@
+describe("Sample Tests", function () {
+
+    it("example", function () {
+        Test.assertEquals(tvRemote("a"), 1);
+        Test.assertEquals(tvRemote("c"), 5);
+        Test.assertEquals(tvRemote("k"), 3);
+        Test.assertEquals(tvRemote("k"), 6);
+        Test.assertEquals(tvRemote("codewars"), 36);
+    });
+
+    it("misc", function () {
+        Test.assertEquals(tvRemote("does"), 16);
+        Test.assertEquals(tvRemote("your"), 23);
+        Test.assertEquals(tvRemote("solution"), 33);
+        Test.assertEquals(tvRemote("work"), 20);
+        Test.assertEquals(tvRemote("for"), 12);
+        Test.assertEquals(tvRemote("these"), 27);
+        Test.assertEquals(tvRemote("words"), 25);
+    });
+
+});
