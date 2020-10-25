32eeb11a53a1ba6e26d602dd26f3b6b6dbdf41e8
Eva Coffield <eva@darcoff.com>
2020-10-25 10:40:52 +0000

    Refactored solution

--- tv-remote.js
+++ tv-remote.js
@@ -11,33 +11,19 @@ var tvRemote = function (word) {
     const characters = word.split('');
     console.log(characters);
     let presses = 0;
-    presses = presses + word.length; //presses += word.length
+    presses += word.length;
 
     characters.forEach(char => {
-        //Get the starting point, row number and column number
         const currentRowNumber = keyboard.findIndex((row) => row.includes(currentCharacter));
         const currentColumnNumber = keyboard[currentRowNumber].findIndex((item) => item === currentCharacter);
 
-        //Get the next character, row and column count
         const nextRowNumber = keyboard.findIndex((row) => row.includes(char));
         const nextColumnNumber = keyboard[nextRowNumber].findIndex((item) => item === char);
-        //console.log([nextRowNumber, nextColumnNumber]);
 
-        //Get the horizontal distance - Use Math.abs to alwasy make them positive values
         const horizontalMoves = Math.abs(nextColumnNumber - currentColumnNumber);
-
-        //Get the vertical distance - Use Math.abs to alwasy make them positive values
         const verticalMoves = Math.abs(nextRowNumber - currentRowNumber);
 
-        console.log([horizontalMoves, verticalMoves]);
-
-        //Add the together
-        const totalMoves = horizontalMoves + verticalMoves;
-
-        //Add them to the presses
-        presses = presses + totalMoves;
-
-        //Reassign current character to char
+        presses = presses + horizontalMoves + verticalMoves;
         currentCharacter = char;
     });
 
